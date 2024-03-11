0182c9e46b2a15a8fdcbb3df875eb119a79b7c75
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-03-10 21:38:45 -0500

    feat: :sparkles: Singleton design pattern

---
+++ src/Singleton/CalculatorHandler.cs
@@ -0,0 +1,67 @@
+namespace Singleton;
+
+public class CalculatorHandler
+{
+    private float Memory { get; set; }
+    private static CalculatorHandler? s_instance;
+
+    private CalculatorHandler() {
+        Memory = 0;
+    }
+
+    public static CalculatorHandler GetInstance()
+    {
+        if (s_instance == null)
+        {
+            s_instance = new CalculatorHandler();
+        }
+        return s_instance;
+    }
+
+    public float GetResult()
+    {
+        return Memory;
+    }
+
+    public CalculatorHandler Clear()
+    {
+        Memory = 0;
+        return this;
+    }
+
+    public CalculatorHandler SetValue(int value)
+    {
+        Memory = value;
+        return this;
+    }
+
+    public CalculatorHandler Add(int value)
+    {
+        Memory += value;
+        return this;
+    }
+
+    public CalculatorHandler Subtract(int value)
+    {
+        Memory -= value;
+        return this;
+    }
+
+    public CalculatorHandler Multiply(int value)
+    {
+        Memory *= value;
+        return this;
+    }
+
+    public CalculatorHandler Divide(int value)
+    {
+        Memory /= value;
+        return this;
+    }
+
+    public CalculatorHandler Modulus(int value)
+    {
+        Memory %= value;
+        return this;
+    }
+}
--- src/Singleton/Program.cs
+++ src/Singleton/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Singleton;
+
+CalculatorHandler calculator1 = CalculatorHandler.GetInstance();
+
+calculator1.SetValue(10)
+    .Add(5)
+    .Multiply(2)
+    .Subtract(10)
+    .Divide(2);
+
+Console.WriteLine(calculator1.GetResult()); // Output: 10
+
+CalculatorHandler calculator2 = CalculatorHandler.GetInstance();
+
+calculator2.Add(8)
+    .Multiply(3)
+    .Subtract(20)
+    .Divide(4);
+
+Console.WriteLine(calculator2.GetResult()); // Output: 8,5
