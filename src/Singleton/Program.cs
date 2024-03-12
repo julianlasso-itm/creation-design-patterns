b2338f2a61955f5401b1cdadf19514d3ad7ba2bf
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-03-11 21:38:35 -0500

    refactor: :recycle: Singleron refactor

--- src/Singleton/Program.cs
+++ src/Singleton/Program.cs
@@ -1,20 +1,24 @@
 ﻿using Singleton;
 
-CalculatorHandler calculator1 = CalculatorHandler.GetInstance();
+public class Program {
+    public static void Main() {
+        CalculatorHandler calculator1 = CalculatorHandler.GetInstance();
 
-calculator1.SetValue(10)
-    .Add(5)
-    .Multiply(2)
-    .Subtract(10)
-    .Divide(2);
+        calculator1.SetValue(10)
+            .Add(5)
+            .Multiply(2)
+            .Subtract(10)
+            .Divide(2);
 
-Console.WriteLine(calculator1.GetResult()); // Output: 10
+        Console.WriteLine(calculator1.GetResult()); // Output: 10
 
-CalculatorHandler calculator2 = CalculatorHandler.GetInstance();
+        CalculatorHandler calculator2 = CalculatorHandler.GetInstance();
 
-calculator2.Add(8)
-    .Multiply(3)
-    .Subtract(20)
-    .Divide(4);
+        calculator2.Add(8)
+            .Multiply(3)
+            .Subtract(20)
+            .Divide(4);
 
-Console.WriteLine(calculator2.GetResult()); // Output: 8,5
+        Console.WriteLine(calculator2.GetResult()); // Output: 8,5
+    }
+}
